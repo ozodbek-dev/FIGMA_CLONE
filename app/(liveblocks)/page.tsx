7bc5f88e35faa992b4aa7470c23a24202208cfa3
ozodbek-dev <ozodbeksoftwaredev@gmail.com>
2024-02-16 14:39:21 +0500

    feature: added Room liveblocks

---
+++ app/(liveblocks)/page.tsx
@@ -0,0 +1,11 @@
+import { Room } from "./Room";
+
+export default function Page() {
+	return (
+		<Room>
+      <h1 className='font-xl'>
+        Lorem ipsum dolor sit amet consectetur.
+      </h1>
+		</Room>
+	);
+}
